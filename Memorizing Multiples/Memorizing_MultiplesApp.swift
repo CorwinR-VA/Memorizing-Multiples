af15b12cb4f8b45916595354756a5684f483c7de
Corwin Rainier <corwinrainier89@gmail.com>
2022-07-17 18:36:02 -0300

    Initial Commit

---
+++ Memorizing Multiples/Memorizing_MultiplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorizing_MultiplesApp.swift
+//  Memorizing Multiples
+//
+//  Created by Corwin Rainier on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorizing_MultiplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
